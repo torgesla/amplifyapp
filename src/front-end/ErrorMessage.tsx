d579fcecedcfaa70d653354396c9fbab5e1dff58
Torgeir Sandnes Laurvik <torgeir.laurvik@gmail.com>
2021-10-27 22:20:00 +0200

    server is running

--- src/front-end/ErrorMessage.tsx
+++ src/front-end/ErrorMessage.tsx
@@ -1,46 +1,44 @@
-import { useEffect, useState } from "react";
-import {zip} from '../helpers';
+import { setServers } from "dns";
+import { useEffect} from "react";
 import styled from 'styled-components'
 
 interface errorProps{
-    error_bools : boolean[],
+    errors : string[],
     showErrors : boolean,
-    doClose : ()=> void
+    doClose : () => void
 }
-const fields : string[]= ['Navn', 'E-post', 'Telefonnummer', 'Postnummer'];
-const expected : string[]= ['Navn Navnesen', 'navn@bedrift.landkode eks. navn@gmail.com', '(+XX)XXX XX XXX eks. 954 32 127', 'XXXX, eks. 0289'];
+const fields_dict : {[id :string] : string[]} = {
+    'name' : ['Navn', 'Navn Navnesen'],
+    'email' : ['E-post', 'navn@bedrift.landkode'],
+    'phone' :['Telefonnummer', '(+XX)XXX XX XXX eks. 954 32 127'],
+    'zip' :  ['Postnummer', 'XXXX']
+}
     
-export default ({error_bools, showErrors, doClose} : errorProps) => {
-    const [errors, setErrors] = useState([] as string[][])
+export default ({errors, showErrors, doClose} : errorProps) => {
     useEffect(() => {
-        const errorCategories : string[] = fields.filter((_, ind) => error_bools[ind])
-        const expectedFormatCategories : string[] = expected.filter((_, ind) => error_bools[ind])
-        setErrors(zip(errorCategories, expectedFormatCategories))
-    }, [error_bools])
+    }, [errors])
     useEffect(() => {
         document.addEventListener("mousedown", () => {
             doClose()
         })
     })
     const StyledModal = styled.div`
-        display: ${showErrors ? 'block' : 'none'}
+        display: ${showErrors ? 'block' : 'none'};
+        position: fixed;
         border-radius: 3px;
         margin: 0.5rem 1rem;
         width: 11rem;
-        background: transparent;
-        color: white;
+        background: white;
+        color: black;
         border: 2px solid white;
     `
-
     return( 
         <StyledModal>
-            <div className="error-list-header">Du har feil format i felt{errors.length===1 ? 'et' : 'ene'}:</div>
+            <div>Du har feil format i felt{errors.length===1 ? 'et' : 'ene'}:</div>
             <ul>
-                {errors.map((error,_) => (
-                    <li className="error-list">
-                        Forventet data på formen: {error[1]} i {error[0].toLowerCase()}-feltet 
-                    </li>
-                ))}
+                {errors.map((error) =>{
+                    <li>{fields_dict[error][0]}</li>
+                })}
             </ul>
         </StyledModal>
     )
--- src/front-end/Form.tsx
+++ src/front-end/Form.tsx
@@ -1,125 +1,155 @@
-import { useState, useEffect} from "react";
+import { useState} from "react";
 import { useForm } from "react-hook-form";
 import { useRecoilState } from "recoil";
 import validator from 'validator';
-import { successSubmitAtom, errorsAtom, windowHeightAtom, windowWidthAtom } from "../atoms";
+import {successSubmitAtom} from "../atoms";
 import {emailRegex} from '../helpers';
 import ErrorMessage from './ErrorMessage';
 import styled from "styled-components";
 
+type FormData = {
+    name : string,
+    email: string,
+    phone: string,
+    areacode: string,
+    comment: string
+}
+const Header = styled.h1`
+position: relative;
+left: 10%;
+font-family: Muli,sans-serif;
+transition: all .2s ease-in-out;
+color: rgb(18,60,105);
+display: inline;
+font-weight: 600;
+font-size: 58px;
+line-height: 1.2;
+padding: 5px 5px 5px 0;
+`
+const FormContainer = styled.div`
+display: flex;
+flex-direction: column;
+background-color: rgb(18,60,105);
+width: 100%;
+height:400px;
+top: 75px;
+position: absolute;
+`
+const FieldSet = styled.fieldset`
+    border-width: 0;
+    border-color: red;
+    height: 100%; 
+`
+const FieldsContainer = styled.div`
+display: flex;
+height: 100%;
+flex-direction: row;
+justify-content: center;
+align-items: center;
+text-align: center;
+`
+const LabelsContainer = styled.div`
+display: flex;
+height: 100%;
+flex-direction: column;
+justify-content: flex-start;
+align-items: center;
+text-align: center;
+`
+const InputsContainer = styled.div`
+display: flex;
+padding: 8px;
+height: 100%;
+flex-direction: column;
+justify-content: space-around;
+`
+const Label = styled.label`
+font-size: 20px;
+align-self: flex-end;
+color: white;
+margin: auto 10px;
 
+`
+const InputField = styled.input`
+height: 20px;
+display: inline;
+width: 300px;
+margin: auto 10px;
+font-family: muli, sans serif;
+`
+const Commentbox = styled.textarea`
+display: inline;
+width: 300px;
+margin: auto 10px;
+font-family: muli, sans serif;;
+`
+const SubmitButton = styled.button`
+text-align: center;
+font-family: Muli,sans-serif;
+margin: 0;
+background-color: #ff5e22;
+display: inline-block;
+box-sizing: border-box;
+border-radius: 10em;
+color: #fff;
+font-size: 18px;
+font-weight: 700;
+padding: 14px 22px;
+margin-top: 10px;
+position: relative;
+text-decoration: none;
+-webkit-backface-visibility: hidden;
+transition: all .3s cubic-bezier(.25,.8,.25,1);
+`
+const defaultFormData = {name:'', email:'', phone:'', areacode:'', comment:''}
 export default () => {
-    const { register, handleSubmit} = useForm();
-    const [showErrors, setshowErrors] = useState<boolean>(false);
+    let errors : string[] = []
     const [succesSubmit, setSuccessSubmit] = useRecoilState(successSubmitAtom);
-    /* const [height, setHeight] =useRecoilState<number>(windowHeightAtom)
-    const [width, setWidth] =useRecoilState<number>(windowWidthAtom) */
-    const [errors, setErrors] = useRecoilState<boolean[]>(errorsAtom);
-    
-    const formValidation = (data : any) => {
-        const nameValid = Boolean(data.name.trim());
-        const epostValid = emailRegex.test(data.email);
-        const phoneValid = validator.isMobilePhone(data.phone);
-        const areacodeValid : boolean =  data.areacode.length == 4;
-        const validsArray = [nameValid,epostValid,phoneValid,areacodeValid];
-        const errorsArray = validsArray.map(x=>!x);
-        const allGood = validsArray.every(x=>x);
-        setErrors(errorsArray)
-        setSuccessSubmit(allGood)
-        setshowErrors(!allGood)        
+    const { register, handleSubmit} = useForm();
+    const [showErrors, setShowErrors] = useState<boolean>(false);
+    /* const [errors, setErrors] = useState<string[]>(['fisk']) */
+    const [formData, setFormData] =useState<FormData>(defaultFormData);
+
+    const sendToValidation = (data : FormData) : Promise<string[]> => {
+        const requestOptions = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(data)
+        };
+        const errors2 : Promise<string[]> = fetch('http://localhost:5000/api', requestOptions)
+            .then(response => response.json())
+            .then(errors => errors);
+        return errors2;
     }
+    const formValidation = async (data : FormData) => {
+
+        const nameValid : string = Boolean(data.name.trim()) ? 'ok':'name' ;
+        const epostValid : string = emailRegex.test(data.email) ? 'ok':'email';
+        const phoneValid : string = validator.isMobilePhone(data.phone) ? 'ok':'phone';
+        const areacodeValid : string =  data.areacode.length == 4 ? 'ok':'zip';
+        const checksArray : string[] = [nameValid, epostValid, phoneValid, areacodeValid];
+        const errorsArray : string[] = checksArray.filter(x => x!== 'ok')
+        const allGood = errorsArray.length === 0;
+
+        if(!allGood){
+            errors =errorsArray;
+            setShowErrors(true);
+            return
+        }
+        errors = await sendToValidation(data)
+        const allGoodbackend : boolean = errors.length === 0;
+        setSuccessSubmit(allGood && allGoodbackend);
+        setShowErrors(true);
+    };
     const clearSubmit = () => {
-        setErrors([])
-        setSuccessSubmit(false)
-        setshowErrors(false)
-    }
-    const Submit = (data : any) => formValidation(data);
-    const Header = styled.h1`
-        position: relative;
-        left: 10%;
-        font-family: Muli,sans-serif;
-        transition: all .2s ease-in-out;
-        color: rgb(18,60,105);
-        display: inline;
-        font-weight: 600;
-        font-size: 58px;
-        line-height: 1.2;
-        padding: 5px 5px 5px 0;
-    `
-    const FormContainer = styled.div`
-        display: flex;
-        flex-direction: column;
-        background-color: rgb(18,60,105);
-        width: 100%;
-        height:400px;
-        top: 75px;
-        position: absolute;
-    `
-    const FieldSet = styled.fieldset`
-        border-width: 2px;
-        border-color: red;
-        height: 100%; 
-    `
-    const FieldsContainer = styled.div`
-        display: flex;
-        height: 100%;
-        flex-direction: row;
-        justify-content: center;
-        align-items: center;
-        text-align: center;
-    `
-    const LabelsContainer = styled.div`
-        display: flex;
-        height: 100%;
-        flex-direction: column;
-        justify-content: flex-start;
-        align-items: center;
-        text-align: center;
-    `
-    const InputsContainer = styled.div`
-        display: flex;
-        padding: 8px;
-        height: 100%;
-        flex-direction: column;
-        justify-content: space-around;
-    `
-
-    const Label = styled.label`
-        font-size: 20px;
-        align-self: flex-end;
-        color: white;
-        margin: auto 10px;
-
-    `
-    const InputField = styled.input`
-        height: 20px;
-        display: inline;
-        width: 300px;
-        margin: auto 10px;
-    `
-    const Commentbox = styled.textarea`
-        display: inline;
-        width: 300px;
-        margin: auto 10px;
-    `
-    const SubmitButton = styled.button`
-        text-align: center;
-        font-family: Muli,sans-serif;
-        margin: 0;
-        background-color: #ff5e22;
-        display: inline-block;
-        box-sizing: border-box;
-        border-radius: 10em;
-        color: #fff;
-        font-size: 18px;
-        font-weight: 700;
-        padding: 14px 22px;
-        margin-top: 10px;
-        position: relative;
-        text-decoration: none;
-        -webkit-backface-visibility: hidden;
-        transition: all .3s cubic-bezier(.25,.8,.25,1);
-    `
+        errors = [];
+        setSuccessSubmit(false);
+        setShowErrors(false);
+    };
+    const Submit = (data : any) => {
+        setFormData(data);
+        formValidation(data);
+    };
     return(
     <>  
         <Header>Spørreskjema - case Torgeir Laurvik</Header>
@@ -129,25 +159,25 @@ export default () => {
                     <legend style={{color:"white", textAlign:"center"}}>Informasjonsskjema</legend>
                     <FieldsContainer>
                         <LabelsContainer>
-                            <Label htmlFor={'name'}>Navn:</Label>
-                            <Label>E-post:</Label>
-                            <Label>Telefon:</Label>
-                            <Label>Postnummer:</Label>
-                            <Label>Kommentar:</Label>
+                            <Label htmlFor={'name'}>Navn</Label>
+                            <Label htmlFor={'email'}>E-post</Label>
+                            <Label htmlFor={'phone'}>Telefon</Label>
+                            <Label htmlFor={'areacode'}>Postnummer</Label>
+                            <Label htmlFor={'comment'}>Kommentar</Label>
                         </LabelsContainer>
                         <InputsContainer>
-                            <InputField {...register("name", {required: true})} name="name" placeholder="Navn Navnesen" autoFocus/>
-                            <InputField {...register("email", {required: true})} name="email" placeholder="navn@domene.no" />
-                            <InputField {...register("phone", {required: true})} name="phone" placeholder="(+XX) XXX XX XXX" />
-                            <InputField {...register("areacode", {required: true})} name="areacode" placeholder="0000" />
-                            <Commentbox {...register('comment')} name="comment"/>
+                            <InputField {...register("name",        {required: true})}  className="required" name="name"        placeholder="Navn Navnesen" autoFocus/>
+                            <InputField {...register("email",       {required: true})}  className="required" name="email"       placeholder="navn@domene.no" />
+                            <InputField {...register("phone",       {required: true})}  className="required" name="phone"       placeholder="(+XX) XXX XX XXX" />
+                            <InputField {...register("areacode",    {required: true})}  className="required" name="areacode"    placeholder="0000" />
+                            <Commentbox {...register('comment')}                                             name="comment"/>
                         </InputsContainer>
                     </FieldsContainer>
                     <SubmitButton type="submit">Send inn!</SubmitButton>
                 </FieldSet>
             </form>
         </FormContainer>
-        {showErrors ? (<ErrorMessage error_bools={errors} showErrors={showErrors} doClose={() => clearSubmit()}/>) : null}
+        {showErrors ? (<ErrorMessage errors={errors} showErrors={showErrors} doClose={() => clearSubmit()}/>) : null}
     </>
     )
 };
