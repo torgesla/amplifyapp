e7b1bfbf82315b2ab2b3c351e1407991b5545b20
Torgeir Sandnes Laurvik <torgeir.laurvik@gmail.com>
2021-10-29 03:15:49 +0200

    okey, this one is the last

--- src/atoms.ts
+++ src/atoms.ts
@@ -7,12 +7,4 @@ const errorsAtom = atom({
     key: 'errors',
     default: [] as boolean[]
 })
-const windowWidthAtom = atom({
-  key: 'width',
-  default: -1 as number
-})
-const windowHeightAtom = atom({
-  key: 'width',
-  default: -1 as number
-})
-export {successSubmitAtom, errorsAtom, windowHeightAtom, windowWidthAtom}
+export {successSubmitAtom, errorsAtom}
